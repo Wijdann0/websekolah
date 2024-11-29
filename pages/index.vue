b445d890605c5757ab0c8d5f8608049c520342db
Wijdann0 <160085539+Wijdann0@users.noreply.github.com>
2024-11-29 12:11:02 +0700

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-4 d-flex justify-content-center">
+        <div class="card mt-5" style="width: 100%;">
+          <img src="../assets/image/cont1.jpeg" class="card-img-top w-50" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-8 mt-5">
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="../assets/image/cont2.jpeg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="../assets/image/cont3.jpeg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="../assets/image/cont4.jpeg" class="d-block w-100" alt="...">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="row bg-secondary text-white text-center mt-5 mb-5 pt-2 pb-2">
+      <div class="col">
+        <h6>Kompetensi Keahlian</h6>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-4 p-4">
+        <div class="card">
+          <h5 class="card-title">pp</h5>
+          <div class="card-body">
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, placeat atque,
+               et veritatis blanditiis voluptatum corrupti repudiandae temporibus quaerat eaque tempora laboriosam
+                ut culpa ad tenetur. Id iste nemo amet!</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-4 p-4">
+        <div class="card">
+          <h5 class="card-title">pp</h5>
+          <div class="card-body">
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, placeat atque,
+               et veritatis blanditiis voluptatum corrupti repudiandae temporibus quaerat eaque tempora laboriosam
+                ut culpa ad tenetur. Id iste nemo amet!</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-4 p-4">
+        <div class="card">
+          <h5 class="card-title">pp</h5>
+          <div class="card-body">
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, placeat atque,
+               et veritatis blanditiis voluptatum corrupti repudiandae temporibus quaerat eaque tempora laboriosam
+                ut culpa ad tenetur. Id iste nemo amet!</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-4 p-4">
+        <div class="card">
+          <h5 class="card-title">pp</h5>
+          <div class="card-body">
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, placeat atque,
+               et veritatis blanditiis voluptatum corrupti repudiandae temporibus quaerat eaque tempora laboriosam
+                ut culpa ad tenetur. Id iste nemo amet!</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-4 p-4">
+        <div class="card">
+          <h5 class="card-title">pp</h5>
+          <div class="card-body">
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, placeat atque,
+               et veritatis blanditiis voluptatum corrupti repudiandae temporibus quaerat eaque tempora laboriosam
+                ut culpa ad tenetur. Id iste nemo amet!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <h6>Prasarana Pembelajaran</h6>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.d-block{
+  height: 500px
+}
+</style>
